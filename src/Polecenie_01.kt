a16c0a121e2af09f2ef26f5890add4df3d274690
Qniara <114557943+Qniara@users.noreply.github.com>
2026-02-03 21:22:31 +0100

    Add polecenie 1

---
+++ src/Polecenie_01.kt
@@ -0,0 +1,12 @@
+fun main() {
+    var a = readln().toInt()
+    var b = readln().toInt()
+    while (a != b) {
+        if (a > b) {
+            a -= b
+        } else {
+            b -= a
+        }
+    }
+    println(a)
+}
